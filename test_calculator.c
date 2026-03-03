c0e6053019f3c95c7102c162f63e2f432b70e571
Adrian Firo <afiro@cpp.edu>
2026-03-03 05:01:11 -0800

    fixed expect comment on test_subtract_positive_numbers, fixed test_subtract_overflow and test_subtract_underflow, and recompiled and ran to get all pass results for tests

--- test_calculator.c
+++ test_calculator.c
@@ -45,30 +45,30 @@ void test_add_underflow(void) {
 }
 
 void test_substract_positive_numbers(void) {
-    TEST_ASSERT_EQUAL(3, subtract(5, 3)); //expect 5 - 3 = 5
+    TEST_ASSERT_EQUAL(2, subtract(5, 3)); //expect 5 - 3 = 2
 }
 
 void test_substract_positive_and_negative_numbers(void) {
-    TEST_ASSERT_EQUAL(4, subtract(2, -1)); //expect 2 - -1 = 3
+    TEST_ASSERT_EQUAL(3, subtract(2, -1)); //expect 2 - -1 = 3
 }
 
 void test_subtract_negative_numbers(void) {
-    TEST_ASSERT_EQUAL(-3, subtract(-6, -2)); //expect -6 - -2 = -4
+    TEST_ASSERT_EQUAL(-4, subtract(-6, -2)); //expect -6 - -2 = -4
 }
 
 void test_subtract_zero(void) {
-    TEST_ASSERT_EQUAL(2, subtract(1, 0)); //expect 1 - 0 = 1
-    TEST_ASSERT_EQUAL(1, subtract(0, 0)); //expect 0 - 0 = 0
+    TEST_ASSERT_EQUAL(1, subtract(1, 0)); //expect 1 - 0 = 1
+    TEST_ASSERT_EQUAL(0, subtract(0, 0)); //expect 0 - 0 = 0
 }
 
 void test_subtract_overflow(void) {
-    int result = subtract(INT_MAX, 1);
-    TEST_ASSERT_TRUE(result < 1); //This checks if overflow occured
+    int result = subtract(INT_MAX, -1);
+    TEST_ASSERT_TRUE(result < 0); //This checks if overflow occured
 }
 
 void test_subtract_underflow(void) {
-    int result = subtract(INT_MIN, -1);
-    TEST_ASSERT_TRUE(result > 1); //This checks if underflow occured
+    int result = subtract(INT_MIN, 1);
+    TEST_ASSERT_TRUE(result > 0); //This checks if underflow occured
 }
 
 int main(void) {
